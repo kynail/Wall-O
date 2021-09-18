974146ebca204413589b4e7207dc4726c2decf2e
Loubna Earally <loubnaearally@MacBook-Pro-de-Loubna.local>
2021-09-18 17:24:22 +0200

    trophies

---
+++ wallo_flutter/lib/widgets/achievement.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Achievement extends StatelessWidget {
+  const Achievement({Key key, this.unlockedImagePath, this.title, this.description, this.lockedImagePath, this.isUnlocked})
+      : super(key: key);
+
+  final String unlockedImagePath;
+  final String lockedImagePath;
+  final String title;
+  final String description;
+  final bool isUnlocked;
+
+  showPopUp(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        title: Text(title),
+        content: Text(description),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => showPopUp(context),
+      child: Image(
+        image: AssetImage(isUnlocked ?
+          unlockedImagePath : lockedImagePath),
+        width: 50,
+      ),
+    );
+  }
+}
